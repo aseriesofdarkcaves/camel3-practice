7b49977b335f4126aff6178afc7b25f7880d156b
aseriesofdarkcaves <aseriesofdarkcaves@protonmail.com>
2021-11-10 16:27:05 +0100

    Add camel-main archetype

---
+++ camel-main/src/main/java/com/asodc/camel3/MyRouteBuilder.java
@@ -0,0 +1,15 @@
+package com.asodc.camel3;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class MyRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:foo?period={{myPeriod}}")
+            .bean("myBean", "hello")
+            .log("${body}")
+            .bean("myBean", "bye")
+            .log("${body}");
+    }
+}
